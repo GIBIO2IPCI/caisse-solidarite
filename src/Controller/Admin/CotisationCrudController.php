4d804b80f72573aa2142bfdec66eea03a2d5ff60
Boye Aurelyne <boyeaurelyne225@gmail.com>
2023-04-19 14:04:29 +0000

    modification de la methode configureActions dans CotisationCrudController

--- src/Controller/Admin/CotisationCrudController.php
+++ src/Controller/Admin/CotisationCrudController.php
@@ -3,8 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Cotisation;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -16,14 +20,31 @@ class CotisationCrudController extends AbstractCrudController
         return Cotisation::class;
     }
 
+    public function configureCrud(Crud $crud):Crud
+   {
+
+    return $crud
+        ->showEntityActionsInlined();
+   }
+
     
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id')->hideOnForm()->hideOnIndex(),
+            IdField::new('id')->hideOnForm(),
             MoneyField::new('montant_cotisation')->setCurrency('XOF'),
+            DateTimeField::new('date_cotisation')->hideOnForm(),
             AssociationField::new('adherent'),
         ];
     }
+
+    public function configureActions(Actions $actions): Actions
+   {
+       return $actions
+           ->update(Crud::PAGE_INDEX, Action::EDIT,
+               fn (Action $action) => $action->setIcon('fa fa-edit')->addCssClass('btn btn-warning'))
+            ->remove(Crud::PAGE_INDEX, Action::DELETE)
+           ;
+   }
     
 }
