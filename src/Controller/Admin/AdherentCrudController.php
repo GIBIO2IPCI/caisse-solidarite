356a72a48d77582b316f9e1d0620572a8a0026c4
Gnaré Ben Ibrahim <bengnareibrahim@gmail.com>
2023-04-19 04:03:27 +0000

    Modification de quelque crud

--- src/Controller/Admin/AdherentCrudController.php
+++ src/Controller/Admin/AdherentCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Adherent;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
@@ -26,17 +29,38 @@ class AdherentCrudController extends AbstractCrudController
     {
 
         return [
-            TextField::new('identifiant')->hideOnForm(),
-            TextField::new('nom'),
-            TextField::new('prenom'),
-            EmailField::new('email'),
-            TelephoneField::new('telephone'),
-            AssociationField::new('site'),
-            AssociationField::new('statut')->hideOnIndex(),
-            AssociationField::new('service')->hideOnIndex(),
-            AssociationField::new('fonction')->hideOnIndex(),
-            DateField::new('date_inscription')->hideOnForm(),
+            TextField::new('identifiant')->hideOnForm()->setColumns(6),
+            TextField::new('nom')->setColumns(6),
+            TextField::new('prenom')->setColumns(6),
+            EmailField::new('email')->setColumns(6),
+            TelephoneField::new('telephone')->setColumns(6),
+            AssociationField::new('site')->setColumns(6),
+            AssociationField::new('statut')->hideOnIndex()->setColumns(6),
+            AssociationField::new('service')->hideOnIndex()->setColumns(6),
+            AssociationField::new('fonction')->hideOnIndex()->setColumns(6),
+            DateField::new('date_inscription')->hideOnForm()->setColumns(6),
         ];
     }
-   
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->update(Crud::PAGE_INDEX, Action::DETAIL,
+             fn (Action $action) => $action->setIcon('fa fa-eye')->addCssClass('btn btn-info'))
+            ->update(Crud::PAGE_INDEX, Action::EDIT,
+                fn (Action $action) => $action->setIcon('fa fa-edit')->addCssClass('btn btn-warning'))
+            ->update(Crud::PAGE_INDEX, Action::DELETE,
+                fn (Action $action) => $action->setIcon('fa fa-trash')->addCssClass('btn btn-danger text-white'))
+            ;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            // ...
+            ->showEntityActionsInlined()
+            ;
+    }
+
 }
--- src/Controller/Admin/SiteAdherentCrudController.php
+++ src/Controller/Admin/SiteAdherentCrudController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\SiteAdherent;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class SiteAdherentCrudController extends AbstractCrudController
 {
@@ -12,14 +17,29 @@ class SiteAdherentCrudController extends AbstractCrudController
         return SiteAdherent::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('libelle'),
         ];
     }
-    */
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->update(Crud::PAGE_INDEX, Action::EDIT,
+                fn (Action $action) => $action->setIcon('fa fa-edit')->addCssClass('btn btn-warning'))
+            ->update(Crud::PAGE_INDEX, Action::DELETE,
+                fn (Action $action) => $action->setIcon('fa fa-trash')->addCssClass('btn btn-danger text-white'))
+            ;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            // ...
+            ->showEntityActionsInlined()
+            ;
+    }
 }
